c4c6d23ddd4ae83ab6e94c2fc1bac7c81e999b45
kirill <143942709+noeuseemo@users.noreply.github.com>
2023-09-30 02:21:14 +0700

    second commit

--- zadacha8.py
+++ zadacha8.py
@@ -1,11 +1,13 @@
-n = input('введите ваше число- ')
- 
-suma = 0
-mult = 1
- 
-for i in n:
-    suma += int(i)
-    mult *= int(i)
- 
-print (f'сумма равняется:  {suma}')
-print (f'произведение будет равно:  {mult}')
+n = int(input("Введите число - "))
+
+a = 0
+b = 1
+
+while n > 0:
+    d = n % 10
+    a = a + d
+    b = b * d
+    n = n // 10
+
+print("Сумма:", a)
+print("Произведение:", b)
